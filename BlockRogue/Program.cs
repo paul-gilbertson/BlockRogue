2a7f29900e615b94a3124e081a129bf89f1cfa03
Paul Gilbertson <paul.gilbertson@gmail.com>
2018-07-07 23:14:51 +0100

    Added Console library and test root console

--- BlockRogue/Program.cs
+++ BlockRogue/Program.cs
@@ -8,14 +8,32 @@ namespace BlockRogue
 {
     class Program
     {
+        private static RLNET.RLRootConsole rConsole;
+
         static void Main(string[] args)
         {
-            //var b = BlockChain.BlockChain.CreateChainFromFile("chain.json");
-            var b = BlockChain.BlockChain.CreateNewChain();
+            var b = BlockChain.BlockChain.CreateChainFromFile("chain.json");
+            //var b = BlockChain.BlockChain.CreateNewChain();
             b.CreateBlock("Test more");
             b.SaveChain("chain.json");
             Console.Out.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(b, Newtonsoft.Json.Formatting.Indented));
+
+            rConsole = new RLNET.RLRootConsole("terminal8x8.png", 100, 70, 8, 8);
+            rConsole.Update += RConsole_Update;
+            rConsole.Render += RConsole_Render;
+            rConsole.Run();
+
             Console.In.ReadLine();
+        }
+
+        private static void RConsole_Render(object sender, RLNET.UpdateEventArgs e)
+        {
+            rConsole.Draw();
+        }
+
+        private static void RConsole_Update(object sender, RLNET.UpdateEventArgs e)
+        {
+            rConsole.Print(10, 10, "Test", RLNET.RLColor.White);
         }
     }
 }
